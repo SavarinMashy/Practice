0a442a59c31889660662155573aedf8b3fe97f43
SavarinMashy <80008736+SavarinMashy@users.noreply.github.com>
2021-06-13 23:59:13 -0400

    Add files via upload

---
+++ reverse_mode.py
@@ -0,0 +1,10 @@
+# Clash of Code
+# The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
+
+# Test 1 --  Input: clash of code, Expected Output: clsh of code
+# Test 2 -- Input: hello world, Expected Output: hello worl
+# Test 3 -- Input: javascript, Expected Output: jvscript
+# Test 4 -- Input: go, Expected Output: o
+
+s = input()
+
